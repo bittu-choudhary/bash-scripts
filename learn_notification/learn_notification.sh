a76057cb4b0f1afe3f538cadf19e27e7f0622917
bittu-choudhary <bittu@moldedbits.com>
2016-12-06 14:56:15 +0530

    adding learn notification bash script

--- learn_notification/learn_notification.sh
+++ learn_notification/learn_notification.sh
@@ -1,5 +1,5 @@
-while IFS=, read col1 col2 col3 col4
+while IFS=, read title subtitle status response
 do
-    osascript -e "display notification \"$col3, $col4\" with title \"$col1\" subtitle \"$col2\""
+    osascript -e "display notification \"$status, $response\" with title \"$title\" subtitle \"$subtitle\""
     sleep 900
 done < workspace/bash-scripts/learn_notification/myfile.csv
