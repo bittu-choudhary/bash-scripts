3e8b742bc66fda4c034618090cdfa3fea93be16f
bittu-choudhary <bittu@moldedbits.com>
2017-05-03 16:21:37 +0530

    add check for git branch and base branch parameter

--- git-new-pr/new-pr.sh
+++ git-new-pr/new-pr.sh
@@ -1,5 +1,11 @@
 repoUrl=`git config --get remote.origin.url`
-removeGitExtension="${repoUrl/.git/""}"
+if [ -z $repoUrl ]; then
+echo "Not a git repo"
+elif [ -z $1 ]; then
+  echo "Pass base branch name. new-pr base-branch-name"
+else
+removeGitExtension="${repoUrl/.git/}"
 branchName=`git rev-parse --symbolic-full-name --abbrev-ref HEAD`
 prUrl="$removeGitExtension/compare/$1...$branchName"
 open -a /Applications/Google\ Chrome.app $prUrl
+fi
