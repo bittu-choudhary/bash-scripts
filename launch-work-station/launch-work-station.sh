220228ca086acc40452e24ca2cb784daebd423eb
bittu-choudhary <bittu@moldedbits.com>
2017-05-03 15:02:16 +0530

    add launch-sys command

---
+++ launch-work-station/launch-work-station.sh
@@ -0,0 +1,9 @@
+open /Applications/Slack.app
+open /Applications/Spotify.app
+open /Users/BittuChoudhary/Applications/Chrome\ Apps.localized/Default\ fhbjgbiflinjbdggehcddcbncdddomop.app
+open -a /Applications/Google\ Chrome.app https://mail.google.com/mail/u/0/ https://github.com/moldedbits https://mail.google.com/mail/u/1/
+open /Applications/sqlitebrowser.app
+open /Applications/iTerm.app
+open /Applications/RubyMine.app
+open /Applications/Evernote.app
+open /Applications/pgAdmin3.app
